e68fff26164912bc69c5d1e15114708476c1b695
kmbrtsy <kemepangs@gmail.com>
2023-11-20 22:43:59 +0800

    first commit

---
+++ src/components/customer/HeroPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Typography, Button, Container } from '@mui/material';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const HeroPage = () => {
+  const carouselImages = [
+    'https://example.com/image1.jpg',
+    'https://example.com/image2.jpg',
+    'https://example.com/image3.jpg',
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Container>
+      <Slider {...settings}>
+        {carouselImages.map((image, index) => (
+          <div key={index} style={{ backgroundImage: `url(${image})`, height: '500px', backgroundSize: 'cover' }}>
+            {/* You can customize the content inside each slide if needed */}
+            <div style={{ textAlign: 'center', paddingTop: '200px', color: 'white' }}>
+              <Typography variant="h2">Welcome to Our E-Commerce Store</Typography>
+              <Typography variant="subtitle1">Discover amazing products and deals</Typography>
+              <Button variant="contained" color="primary" size="large">
+                Shop Now
+              </Button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </Container>
+  );
+};
+
+export default HeroPage;
