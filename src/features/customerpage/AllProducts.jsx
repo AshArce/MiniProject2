29f7f09968c64b1808b2c0b21031d0009d0665f2
AshArce <arcebuchejohnashley@gmail.com>
2023-11-24 20:09:19 +0800

    working shopingcart

--- src/features/customerpage/AllProducts.jsx
+++ src/features/customerpage/AllProducts.jsx
@@ -1,15 +1,31 @@
-// AllProducts.jsx
+// Import necessary React components and Material-UI components
 import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button'; // Import Button component
 import ProductCard from '../../components/customer/ProductCard';
-import ShoppingCart from '../../components/customer/ShopingCart';
+
 import { sampleProducts } from '../adminpage/productdata';
 import ProductsNav from '../../components/customer/ProductsNav';
+import CartModal from '../../components/customer/CartModal';
+
 
 function AllProducts() {
   const [cart, setCart] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState(sampleProducts);
   const [selectedCategoryFirst, setSelectedCategoryFirst] = useState('');
   const [selectedCategorySecond, setSelectedCategorySecond] = useState('');
+
+  // State to manage the visibility of the shopping cart modal
+  const [isCartModalOpen, setCartModalOpen] = useState(false);
+
+  // Function to open the shopping cart modal
+  const openCartModal = () => {
+    setCartModalOpen(true);
+  };
+
+  // Function to close the shopping cart modal
+  const closeCartModal = () => {
+    setCartModalOpen(false);
+  };
 
   const addToCart = (product) => {
     const existingProduct = cart.find((item) => item.id === product.id);
@@ -22,6 +38,18 @@ function AllProducts() {
     } else {
       setCart((prevCart) => [...prevCart, { ...product, quantity: product.quantity }]);
     }
+  };
+
+  const removeFromCart = (productId) => {
+    const updatedCart = cart.filter((item) => item.id !== productId);
+    setCart(updatedCart);
+  };
+
+  const updateQuantity = (productId, newQuantity) => {
+    const updatedCart = cart.map((item) =>
+      item.id === productId ? { ...item, quantity: newQuantity } : item
+    );
+    setCart(updatedCart);
   };
 
   useEffect(() => {
@@ -47,6 +75,19 @@ function AllProducts() {
         setSelectedCategorySecond={setSelectedCategorySecond}
       />
       <ProductCard products={filteredProducts} addToCart={addToCart} />
+      {/* Button to open the shopping cart modal */}
+      <Button variant="contained" color="primary" onClick={openCartModal}>
+        Open Shopping Cart
+      </Button>
+
+      {/* Shopping Cart Modal */}
+      <CartModal
+        isOpen={isCartModalOpen}
+        onClose={closeCartModal}
+        cart={cart}
+        removeFromCart={removeFromCart}
+        updateQuantity={updateQuantity}
+      />
     </>
   );
 }
